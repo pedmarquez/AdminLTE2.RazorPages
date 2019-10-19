7185fe99c9eada2260af9ad86bef45481119f418
Pedro Marquez <pedro_marquez@outlook.com>
2019-10-19 16:59:49 -0300

    Add project files.

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'node_modules/admin-lte/dist',
+                src: '**',
+                dest: 'wwwroot/'
+            },
+            components: {
+                expand: true,
+                cwd: 'node_modules/admin-lte/bower_components',
+                src: '**',
+                dest: 'wwwroot/lib/'
+            }
+        },
+    });
+    //grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+};
