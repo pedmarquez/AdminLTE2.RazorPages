647d894dab79c588ae5436ef8e5a13fd7849c7f4
Pedro Marquez <pedro_marquez@outlook.com>
2019-10-25 20:36:14 -0300

    Add InfoBox ViewComponent

---
+++ Model/InfoBoxModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AdminLTE2.Web.Model
+{
+    public class InfoBoxModel
+    {
+        public string Icon { get; set; }
+        public string BackgroundBox { get; set; }
+        public string BackgroundIcon { get; set; }
+        public string Text { get; set; }
+        public string Number { get; set; }
+
+        public int? ProgressValue { get; set; }
+        public string ProgressDescription { get; set; }
+    }
+}
---
+++ ViewComponents/InfoBoxViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using AdminLTE2.Web.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AdminLTE2.Web.ViewComponents
+{
+    public class InfoBoxViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string icon, string backgroundBox, string backgroundIcon, string text, string number, int? progressValue, string progressDescription)
+        {
+            return await Task.Run(() => View(new InfoBoxModel { Icon = icon, BackgroundBox = backgroundBox, BackgroundIcon = backgroundIcon, Text = text, Number = number, ProgressValue = progressValue, ProgressDescription = progressDescription }));
+        }
+    }
+}
